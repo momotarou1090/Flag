82a359b085cc8cc80683ad3831c2fb36cc9f4384
Your Name <you@example.com>
2023-11-07 04:28:29 +0900

    Prompt changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.hosts << "flag-forest.com"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
